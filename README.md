34eb6ae29188e7aa3278db138a23d72cbd56277d
Pablo Estigarribia <pablo.estigarribia@visitor.upm.com>
2018-03-14 16:48:54 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Introduction
+============
+
+Draft to provide one simple and one complete example to develop nagios plugins with python
+
+Existing tutorials looks old
+Existing modules like:
+http://pythonhosted.org/nagiosplugin/tutorial/check_load.html
+http://python-nagios-helpers.readthedocs.io/en/latest/intro.html
+
+And also pynag, looks old, doesn't fully supports python3 in some cases or they are too complicated to create plugins. Also some of them add ugly and big dependencies.
+
+Basic
+=====
+
+See [basic](basic/basic_plugin.py)
+
+Complete
+========
+
+Not implemented yet, but will use example base: https://github.com/pablodav/sharepoint_health_mon_plugin
---
+++ basic/basic_plugin.py
@@ -0,0 +1,68 @@
+#!python3
+# https://nagios-plugins.org/doc/guidelines.html
+
+# Import required libs for your plugin
+# from x import y
+
+# Return codes expected by Nagios
+OK = 0
+WARNING = 1
+CRITICAL = 2
+UNKNOWN = 3
+
+# Return message
+message = {
+    'status': OK,
+    'summary': 'Example summary',
+    'perfdata': 'label1=0;;;; '  # 'label'=value[UOM];[warn];[crit];[min];[max] 
+}
+
+# For multiple perdata, ensure to add space after each perfdata
+# message['perfdata'] = 'label1=x;;;; '
+# message['perfdata'] += 'label2=x;;;; '
+
+def collect_data():
+    ### some code here
+    ### or calling external modules
+    data = result_data
+    return data
+
+def check(data):
+    """
+    : param: example with data as int with a simple number to compare
+    return: code status number (0, 1, 2 ,3) (See lines above)
+    """
+    ## Example check 
+    pc = len(data)
+    if pc >= 2:
+        status = CRITICAL
+    elif pc == 1:
+        status = WARNING
+    else:
+        status = OK
+    return status
+
+
+# Check logic starts here
+data = collect_data()
+message['status'] = check(data)
+# Add summary - you can also use more complex functions
+message['summary'] = "SharepointHealth"
+# Add perfdata - you can also use more complex functions
+total = len(data)
+message['perfdata'] = "alerts={};1;2;0; ".format(total)
+# For multiple perdata, ensure to add space after each perfdata
+# message['perfdata'] = 'label1=x;;;; '
+# message['perfdata'] += 'label2=x;;;; '
+
+# Print the message for nagios with perfdata
+print("{summary}|{perfdata}".format(
+    summary=message.get('summary'),
+    perfdata=message.get('perfdata')
+))
+
+# Exit with status code
+raise SystemExit(message['status'])
+
+# Adding Argument parser, see:
+# https://docs.python.org/3.5/library/argparse.html
